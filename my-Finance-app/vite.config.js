fc512c9b57dfdfc482f9f8217ef0ea6ab9bdd2dd
unknown <dwiprajdey33720@gmail.com>
2025-06-15 10:05:09 +0530

    Budjet Wow Tracker

--- my-Finance-app/vite.config.js
+++ my-Finance-app/vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   build:{
+    ontDir:"dist",
+
+   },
+   server:{
+    host:true,
+   }
 })
 
